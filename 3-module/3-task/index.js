0e3f3b035dfc212743b919a806cf92e20b80b867
marina210 <BalmakovaMV@alrosa.ru>
2022-12-06 00:43:00 +0900

    Mодуль 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,21 @@
 function camelize(str) {
-  // ваш код...
+    let arr = str.split('');
+ let y = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '-') {
+        y = 1;
+       continue;
+     } else {
+       if (y === 1) {    // признак того, что пред.символ - тире
+        arr[i] = arr[i].toUpperCase() ;
+        y = 0;
+      } 
+     }
+     
+   }
+   
+return arr
+   .join('')
+   .split('-')
+   .join('')
 }
