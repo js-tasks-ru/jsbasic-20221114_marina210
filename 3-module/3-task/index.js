b80d85aec827a010711af972d199e5e75acb7374
marina210 <BalmakovaMV@alrosa.ru>
2022-12-07 23:33:45 +0900

    Mодуль 3 Исправлена задача 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,21 +1,7 @@
 function camelize(str) {
-    let arr = str.split('');
- let y = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === '-') {
-        y = 1;
-       continue;
-     } else {
-       if (y === 1) {    // признак того, что пред.символ - тире
-        arr[i] = arr[i].toUpperCase() ;
-        y = 0;
-      } 
-     }
-     
-   }
-   
-return arr
-   .join('')
-   .split('-')
-   .join('')
+    return str
+      .split('-')
+      .map((str0,ind) => (ind>0) ? (str0[0].toUpperCase() + str0.slice(1)) : str0)
+      .join('');
+
 }
